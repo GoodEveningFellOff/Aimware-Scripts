c4139413297b4152f488aaa41e9a478ededeac17
Good Evening <114830237+GoodEveningFellOff@users.noreply.github.com>
2024-01-31 13:07:15 -0500

    Update CS2 Custom Hitsound.lua

--- CS2 Custom Hitsound.lua
+++ CS2 Custom Hitsound.lua
@@ -1,12 +1,36 @@
 local guiRef = gui.Reference("Misc", "General", "Extra");
 
-local guiHitmarker = gui.Editbox(guiRef, "hitsound", "Hitsound File");
-guiHitmarker:SetDescription("Name of the .vsnd_c file placed in the sounds folder.");
+local guiSelection = gui.Combobox(guiRef, "hitsound", "Hitsound", "Hide", "Show General Settings", "Show File Inputs");
+guiSelection:SetDescription("Name of the .vsnd_c file placed in the sounds folder.");
 
-local guiVolume = gui.Slider(guiRef, "hitsound_volume", "Hitsound Volume", 1, 0, 1.5, 0.01);
-guiVolume:SetDescription("Volume of the hitsound.");
+local guiRepeatFix = gui.Checkbox(guiRef, "hitsound.repeatfix", "Repeat Fix", false);
+guiRepeatFix:SetDescription("Prevent sounds repeating on the same tick.");
+guiRepeatFix:SetInvisible(true);
+
+local guiVolume = gui.Slider(guiRef, "hitsound.volume", "Volume", 1, 0, 1.5, 0.01);
+guiVolume:SetDescription("Volume of the sounds.");
+guiVolume:SetInvisible(true)
+
+local guiHitsound = gui.Editbox(guiRef, "hitsound.hit", "Hitsound File");
+guiHitsound:SetDescription("Sound played when you hit someone.");
+guiHitsound:SetInvisible(true);
+
+local guiKillsound = gui.Editbox(guiRef, "hitsound.kill", "Killsound File");
+guiKillsound:SetDescription("Sound played when you kill someone.");
+guiKillsound:SetInvisible(true);
+
+local g_bRepeatFix = false;
+local g_flVolume = 0;
+local g_sHitsound = false;
+local g_sKillsound = false;
+
+local g_iVisibility = 0;
 
 local g_iLastHitTick = 0;
+local g_iLastKillTick = 0;
+
+local g_iTickCount = 0;
+
 callbacks.Register("FireGameEvent", function(ctx)
     if ctx:GetName() ~= "player_hurt" then
         return;
@@ -37,12 +61,50 @@ callbacks.Register("FireGameEvent", function(ctx)
         return;
     end
 
-    if globals.TickCount() == g_iLastHitTick then
+    if ctx:GetInt("health") > 0 then
+        if (g_bRepeatFix and g_iLastHitTick == g_iTickCount) or not g_sHitsound then
+            return;
+        end
+
+        g_iLastHitTick = g_iTickCount;
+
+        client.SetConVar("snd_toolvolume", g_flVolume, true);
+        client.Command(g_sHitsound, true)
+    else
+        if (g_bRepeatFix and g_iLastKillTick == g_iTickCount) or not g_sKillsound then
+            return;
+        end
+
+        g_iLastKillTick = g_iTickCount;
+
+        client.SetConVar("snd_toolvolume", g_flVolume, true);
+        client.Command(g_sKillsound, true)
+    end
+end)
+
+callbacks.Register("Draw", function()
+    if g_iTickCount ~= globals.TickCount() then
+        g_iTickCount = globals.TickCount();
+
+        g_bRepeatFix = guiRepeatFix:GetValue();
+        g_flVolume = guiVolume:GetValue() or 0;
+
+        g_sHitsound = guiHitsound:GetValue() or '';
+        g_sHitsound = (g_sHitsound:match("%w")) and ("play sounds\\" .. g_sHitsound) or false;
+        
+        g_sKillsound = guiKillsound:GetValue() or '';
+        g_sKillsound = (g_sKillsound:match("%w")) and ("play sounds\\" .. g_sKillsound) or false;
+    end
+
+    local iVisibility = guiSelection:GetValue();
+    if iVibility == g_iVisibility then
         return;
     end
 
-    g_iLastHitTick = globals.TickCount();
+    g_iVisibility = iVisibility;
+    guiRepeatFix:SetInvisible(iVisibility ~= 1);
+    guiVolume:SetInvisible(iVisibility ~= 1);
 
-    client.SetConVar("snd_toolvolume", guiVolume:GetValue() or 1, true);
-    client.Command(("play sounds\\%s"):format(guiHitmarker:GetValue() or ' '), true)
+    guiHitsound:SetInvisible(iVisibility ~= 2);
+    guiKillsound:SetInvisible(iVisibility ~= 2);
 end)
