042356072fa4142acb6476637fdd5c4f76366e5c
Michtar <59453471+michtarxd@users.noreply.github.com>
2024-06-25 11:14:12 +0200

    Optimize Auto Defuse Lua

--- CS2 Auto Defuse.lua
+++ CS2 Auto Defuse.lua
@@ -1,78 +1,32 @@
-local guiEnabled = gui.Checkbox(gui.Reference("Misc", "General", "Extra"), "autodefuse", "Auto-Defuse", false);
-guiEnabled:SetDescription("Defuses the bomb right before time runs out.");
+local guiEnabled = gui.Checkbox(gui.Reference("Misc", "General", "Extra"), "autodefuse", "Auto-Defuse", false)
+guiEnabled:SetDescription("Defuses the bomb right before time runs out.")
 
-local g_bIsDefusing = false;
+local IN_USE = bit.lshift(1, 5)
+callbacks.Register("CreateMove", function(pCmd)
+    if not pCmd then return end
 
-local function StopDefusing()
-    if g_bIsDefusing then
-        client.Command("-use");
-        g_bIsDefusing = false;
-    end
-end
+    if not guiEnabled:GetValue() then return end
 
-callbacks.Register("Draw", function()
-    if not guiEnabled:GetValue() then
-        return StopDefusing();
-    end
+    local pLocal = entities.GetLocalPlayer()
+    if not pLocal or not pLocal:IsAlive() or pLocal:GetTeamNumber() ~= 3 then return end
 
-    local pLocalPlayer = entities.GetLocalPlayer();
-    if not pLocalPlayer then
-        return StopDefusing();
-    end
-
-    if not pLocalPlayer:IsAlive() or pLocalPlayer:GetTeamNumber() ~= 3 then
-        return StopDefusing();
-    end
-
-    local iLocalIndex = pLocalPlayer:GetIndex();
-    local pLocalPlayerController = nil;
-    for _, pEnt in pairs(entities.FindByClass("CCSPlayerController")) do
-        local pPawn = pEnt:GetPropEntity("m_hPlayerPawn");
-
-        if pPawn:GetIndex() == iLocalIndex then
-            pLocalPlayerController = pEnt;
-            break;
-        end
-    end
-
-    if not pLocalPlayerController then
-        return StopDefusing();
-    end
+    local pLocalPlayerController = nil
+    if (pLocal:GetPropInt("m_hOriginalController") > 0) then pLocalPlayerController = pLocal:GetPropEntity("m_hOriginalController") end
+    if not pLocalPlayerController then return end
 
     local pBomb = nil
-    for _, pEnt in pairs(entities.FindByClass("C_PlantedC4")) do
-        if pEnt:GetPropBool("m_bBombTicking") then
-            pBomb = pEnt;
-        end
-    end
+    for _, pEnt in pairs(entities.FindByClass("C_PlantedC4")) do if pEnt:GetPropBool("m_bBombTicking") then pBomb = pEnt end end
+    if not pBomb then return end
+    if (pBomb:GetAbsOrigin() - pLocal:GetAbsOrigin()):Length() > 65 then return end
 
-    if not pBomb then
-        return StopDefusing();
-    end
+    local pBombDefuser = nil
+    if (pBomb:GetPropInt("m_pBombDefuser") > 0) then pBombDefuser = pBomb:GetPropEntity("m_pBombDefuser") end
+    if pBombDefuser and pBombDefuser:GetIndex() ~= pLocal:GetIndex() then return end
 
-    if pLocalPlayerController:GetClass() ~= "CCSPlayerController" or pBomb:GetClass() ~= "C_PlantedC4" then
-        return StopDefusing();
-    end
+    local flDefuseLength = (pLocalPlayerController:GetPropBool("m_bPawnHasDefuser") and 5 or 10)
+    local flTimeUntilDefuse = pBomb:GetPropFloat("m_flC4Blow") - globals.CurTime() - flDefuseLength
+    flTimeUntilDefuse = flTimeUntilDefuse - (globals.FrameTime() + 0.015625 + (pLocalPlayerController:GetPropInt("m_iPing") * 2 / 1000))
+    if flTimeUntilDefuse > 0 then return end
 
-    if (pBomb:GetPropBool("m_bBeingDefused") and not g_bIsDefusing) or (pBomb:GetAbsOrigin() - pLocalPlayer:GetAbsOrigin()):Length() > 65 then
-        return StopDefusing();
-    end
-
-    if g_bIsDefusing then
-        return;
-    end
-
-    local flRemainingBombTime = pBomb:GetPropFloat("m_flC4Blow") - globals.CurTime() - (pLocalPlayerController:GetPropBool("m_bPawnHasDefuser") and 5 or 10);
-    flRemainingBombTime = flRemainingBombTime - (globals.FrameTime() + 0.015625 + (pLocalPlayerController:GetPropInt("m_iPing") * 2 / 1000));
-
-    if flRemainingBombTime < 0 or flRemainingBombTime > math.max(0.01, globals.FrameTime()) then
-        return StopDefusing();
-    end
-
-    client.Command("+use");
-    g_bIsDefusing = true;
-end)
-
-callbacks.Register("Unload", function()
-    StopDefusing();
+    pCmd:SetButtons(pCmd:GetButtons() + IN_USE)
 end)
