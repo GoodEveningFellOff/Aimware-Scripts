2a22e087f82836e6842e19cc55b527c231557c9b
Good Evening <114830237+GoodEveningFellOff@users.noreply.github.com>
2024-08-06 12:44:17 -0400

    Update CS2 GetBombRadiusFn.lua

--- Utils/CS2 GetBombRadiusFn.lua
+++ Utils/CS2 GetBombRadiusFn.lua
@@ -1,12 +1,15 @@
 local m = {
-	-- Updated May-20th-2024 @11:25 EST
+	-- Updated August-6th-2024 @12:42 EST
 	-- map_showbombradius || bombradius @ game/csgo/maps/<map>.vpk/entities/default_ents.vents_c ## only lists if value is overwritten
 	["maps/de_ancient.vpk" ] = 650 * 3.5;
 	["maps/de_anubis.vpk"  ] = 450 * 3.5;
+	["maps/de_assembly.vpk"] = 500 * 3.5;
 	["maps/de_inferno.vpk" ] = 620 * 3.5;
+	["maps/de_mills.vpk"   ] = 500 * 3.5;
 	["maps/de_mirage.vpk"  ] = 650 * 3.5;
 	["maps/de_nuke.vpk"    ] = 650 * 3.5;
 	["maps/de_overpass.vpk"] = 650 * 3.5;
+	["maps/de_thera.vpk"   ] = 500 * 3.5;
 	["maps/de_vertigo.vpk" ] = 500 * 3.5;
 };
 
